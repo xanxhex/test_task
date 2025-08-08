72111b4f39e52c2c78cedb22ed7734558fb7f677
Sasha <xanxhexworld@gmail.com>
2025-08-09 01:40:43 +0300

    fix

--- abz.agency/vite.config.js
+++ abz.agency/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<test-task>/',
+   base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

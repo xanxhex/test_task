a9c3a2c971e272f3d912849c1d32ef0f5ad0438e
Sasha <xanxhexworld@gmail.com>
2025-08-09 02:34:33 +0300

    fix

--- abz.agency/vite.config.js
+++ abz.agency/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-   base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@api": path.resolve(__dirname, "./src/api"),
-      "@containers": path.resolve(__dirname, "./src/containers"),
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@containers': path.resolve(__dirname, './src/containers'),
     },
   },
-});
+})
